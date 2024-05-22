8f55f60d8e2ab0d42c0eb18312487f123da3f8b1
bharath <bwaj9078@gmail.com>
2024-05-22 16:10:39 +0530

    third push

--- src/Contact.js
+++ src/Contact.js
@@ -8,7 +8,9 @@ const Contact = () => {
       <img src={phonecallImage} alt="phonecallImage" style={{ width: '100%', height: 'auto', objectFit:'cover' }} />
       <Typography variant='h6'><strong>We’d love to hear from you.</strong> </Typography>
       <Typography variant='h6'><strong>Drop us an email at</strong></Typography>
-      <Typography variant='h6'><strong>askus@radush.io</strong></Typography>
+     <a  href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=askus@radush.io" 
+        target="_blank" 
+        rel="noopener noreferrer" > <Typography variant='h6'><strong>askus@radush.io</strong></Typography></a>
       <div>
         <Typography variant='h6' sx={{marginTop:'20px'}}><strong>Our Locations</strong></Typography>
       <Typography variant='h6'><strong>India :- +91 6366094725
