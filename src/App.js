73daa8225d377242bdaa00d0ce7902851b021430
Veera Chinna Perumal Velusamy <vveera8124@gmail.com>
2024-05-22 15:14:44 +0530

    update the logo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@
 // import Footer from './Footer'; // Import the Footer component
 // import './App.css';
 
-
 // function App() {
 //   return (
 //     <div className="App">
@@ -23,33 +22,30 @@
 
 // export default App;
 
-
-
-
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './Navbar';
-import Home from './Home'; // Import the Home component
-import About from './About'; // Import the About component
-import Services from './Services'; // Import the Services component
-import GlobalPresence from './GlobalPresence'; // Import the GlobalPresence component
-import Contact from './Contact'; // Import the Contact component
-import ContentCorner from './ContentCorner'; // Import the ContentCorner component
-import Footer from './Footer'; // Import the Footer component
-import TermsAndConditions from './TermsAndConditions';
-import PrivacyStatement from './PrivacyStatement';
-import RefundPolicy from './RefundPolicy';
-import SLO from './SLO';
-import ErrorBudgets from './ErrorBudgets';
-import FaultTolerance from './FaultTolerance';
-import CanaryReleases from './CanaryReleases';
-import FramingErrorBudget from './FramingErrorBudget';
-import FalsePositives from './FalsePositives';
-import BreakingDownSilos from './BreakingDownSilos';
-import RealWorldSimulation from './RealWorldSimulation';
-import DevOps from './DevOps';
-
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./Navbar";
+import Home from "./Home"; // Import the Home component
+import About from "./About"; // Import the About component
+import Services from "./Services"; // Import the Services component
+import GlobalPresence from "./GlobalPresence"; // Import the GlobalPresence component
+import Contact from "./Contact"; // Import the Contact component
+import ContentCorner from "./ContentCorner"; // Import the ContentCorner component
+import Footer from "./Footer"; // Import the Footer component
+import TermsAndConditions from "./TermsAndConditions";
+import PrivacyStatement from "./PrivacyStatement";
+import RefundPolicy from "./RefundPolicy";
+import SLO from "./SLO";
+import ErrorBudgets from "./ErrorBudgets";
+import FaultTolerance from "./FaultTolerance";
+import CanaryReleases from "./CanaryReleases";
+import FramingErrorBudget from "./FramingErrorBudget";
+import FalsePositives from "./FalsePositives";
+import BreakingDownSilos from "./BreakingDownSilos";
+import RealWorldSimulation from "./RealWorldSimulation";
+import DevOps from "./DevOps";
+import { Container } from "@mui/material";
+import "./App.css";
 // import SLO from './SLO';
 // import ErrorBudgets from './ErrorBudgets';
 // import FaultTolerance from './FaultTolerance';
@@ -57,38 +53,41 @@ import './App.css';
 
 function App() {
   return (
-    
-      <div className="App">
-        <Navbar />
+    <div className="App">
+      <Navbar />
+      <Container sx={{ width: "100%" }}>
         <Routes>
           <Route path="/*" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
-          
+
           <Route path="/global-presence" element={<GlobalPresence />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/content-corner" element={<ContentCorner />} />
-          <Route path="/terms-conditions" element={<TermsAndConditions/>} />
-          <Route path="/privacy-statement" element={<PrivacyStatement/>} />
-          <Route path="/refund-policy" element={<RefundPolicy/>} />
-          <Route path="/slo" element={<SLO/>} />
-          <Route path="/error-budgets" element={<ErrorBudgets/>} />
-          <Route path="/fault-tolerance" element={<FaultTolerance/>} />
-          <Route path="/canary-releases" element={<CanaryReleases/>} />
-          <Route path="/framing-error-budgets" element={<FramingErrorBudget/>} />
-          <Route path="/false-positives" element={<FalsePositives/>} />
-          <Route path="/breaking-down-silos" element={<BreakingDownSilos/>} />
-          <Route path="/real-world-simulation" element={<RealWorldSimulation/>} />
-          <Route path="/dev-ops" element={<DevOps/>} />
+          <Route path="/terms-conditions" element={<TermsAndConditions />} />
+          <Route path="/privacy-statement" element={<PrivacyStatement />} />
+          <Route path="/refund-policy" element={<RefundPolicy />} />
+          <Route path="/slo" element={<SLO />} />
+          <Route path="/error-budgets" element={<ErrorBudgets />} />
+          <Route path="/fault-tolerance" element={<FaultTolerance />} />
+          <Route path="/canary-releases" element={<CanaryReleases />} />
+          <Route
+            path="/framing-error-budgets"
+            element={<FramingErrorBudget />}
+          />
+          <Route path="/false-positives" element={<FalsePositives />} />
+          <Route path="/breaking-down-silos" element={<BreakingDownSilos />} />
+          <Route
+            path="/real-world-simulation"
+            element={<RealWorldSimulation />}
+          />
+          <Route path="/dev-ops" element={<DevOps />} />
           {/* Define routes for other pages/components as needed */}
-          
         </Routes>
-       
-        <Footer /> {/* Include the Footer component here */}
-      </div>
-   
+      </Container>
+      <Footer /> {/* Include the Footer component here */}
+    </div>
   );
 }
 
 export default App;
-
--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,30 @@
-import React from 'react';
-import Carousel from 'react-material-ui-carousel';
-import { Paper, Typography, Grid } from '@mui/material';
-import './App.css'; // Import your CSS file for styling
-import image1 from './assets/Product-Engineering.png';
-import image2 from './assets/Partnering for Success.jpg';
-import image3 from './assets/Consulting Services.jpg';
-import image4 from './assets/Training Services.jpg';
-import image5 from './assets/Embeded Systems.jpg';
-import image6 from './assets/ESG.jpg';
-import officeBuildingImage from './assets/office-building-q4i5wygbi2h3068rajo6d22rm0vhsgbhx4eth48oa0.png';
-import timelineImage from './assets/timeline-q4i616q6989j903kldhqkznfug0yff40i23h7xywa0.png';
-import meetingImage from './assets/meeting-1-q4i64tdqn78e2stsefyvpkoif04z5cipc0ryxel694.png';
-import { Link, useNavigate } from 'react-router-dom';
+import React from "react";
+import Carousel from "react-material-ui-carousel";
+import { Paper, Typography, Grid } from "@mui/material";
+import "./App.css"; // Import your CSS file for styling
+import image1 from "./assets/Product-Engineering.png";
+import image2 from "./assets/Partnering for Success.jpg";
+import image3 from "./assets/Consulting Services.jpg";
+import image4 from "./assets/Training Services.jpg";
+import image5 from "./assets/Embeded Systems.jpg";
+import image6 from "./assets/ESG.jpg";
+import officeBuildingImage from "./assets/office-building-q4i5wygbi2h3068rajo6d22rm0vhsgbhx4eth48oa0.png";
+import timelineImage from "./assets/timeline-q4i616q6989j903kldhqkznfug0yff40i23h7xywa0.png";
+import meetingImage from "./assets/meeting-1-q4i64tdqn78e2stsefyvpkoif04z5cipc0ryxel694.png";
+import { Link, useNavigate } from "react-router-dom";
 
 const items = [
   {
     image: image2,
-    
   },
   {
     image: image1,
-    
   },
   {
     image: image3,
-   
   },
   {
     image: image4,
-    
   },
 ];
 
@@ -36,7 +32,7 @@ const Home = () => {
   const navigate = useNavigate();
   return (
     <div>
-     {/* <Grid container spacing={2} justifyContent="center" style={{}}>
+      {/* <Grid container spacing={2} justifyContent="center" style={{}}>
           <Grid item xs={12}>
       <Carousel>
         {items.map((item, index) => (
@@ -49,45 +45,90 @@ const Home = () => {
       </Grid>
       </Grid> */}
       <Grid justifyContent="center">
-      <Grid item xs={11} sm={10} md={8} lg={6}>
-        <Carousel>
-          {items.map((item, index) => (
-            <div key={index}>
-              <Paper>
-                <img
-                  src={item.image}
-                  alt={`Image ${index + 1}`}
-                  style={{ width: '100%', height: 'auto', maxHeight: '600px', objectFit: 'cover' }}
-                />
-              </Paper>
-            </div>
-          ))}
-        </Carousel>
+        <Grid item xs={11} sm={10} md={8} lg={6}>
+          <Carousel>
+            {items.map((item, index) => (
+              <div key={index}>
+                <Paper>
+                  <img
+                    src={item.image}
+                    alt={`Image ${index + 1}`}
+                    style={{
+                      width: "100%",
+                      height: "auto",
+                      maxHeight: "600px",
+                      objectFit: "cover",
+                    }}
+                  />
+                </Paper>
+              </div>
+            ))}
+          </Carousel>
+        </Grid>
       </Grid>
-    </Grid>
       <div className="service-background">
-        <Typography variant="h4" align="center" gutterBottom style={{ color: 'black', fontWeight: 'bold' }}>
+        <Typography
+          variant="h4"
+          align="center"
+          gutterBottom
+          style={{ color: "black", fontWeight: "bold" }}
+        >
           Services
         </Typography>
-        <Grid container spacing={2} justifyContent="center" style={{marginTop:'40px'}}>
+        <Grid
+          container
+          spacing={2}
+          justifyContent="center"
+          style={{ marginTop: "40px" }}
+        >
           <Grid item xs={12}>
             <Grid container justifyContent="center" spacing={2}>
               <Grid item>
                 <div className="service1-item-container">
                   <img src={image1} alt="Service 1" className="service-image" />
-                  <Typography variant="body1" align="center" style={{ color: 'black', fontWeight: 'bold',fontSize:'18px' }}>Software solutions and Product re-engineering</Typography>
+                  <Typography
+                    variant="body1"
+                    align="center"
+                    style={{
+                      color: "black",
+                      fontWeight: "bold",
+                      fontSize: "18px",
+                    }}
+                  >
+                    Software solutions and Product re-engineering
+                  </Typography>
                 </div>
               </Grid>
               <Grid item>
                 <div className="service1-item-container">
                   <img src={image3} alt="Service 2" className="service-image" />
-                  <Typography variant="body1" align="center" style={{ color: 'black', fontWeight: 'bold',fontSize:'18px' }}>Consulting</Typography>
+                  <Typography
+                    variant="body1"
+                    align="center"
+                    style={{
+                      color: "black",
+                      fontWeight: "bold",
+                      fontSize: "18px",
+                    }}
+                  >
+                    Consulting
+                  </Typography>
                 </div>
               </Grid>
               <Grid item>
                 <div className="service1-item-container">
                   <img src={image4} alt="Service 3" className="service-image" />
-                  <Typography variant="body1" align="center" style={{ color: 'black', fontWeight: 'bold',fontSize:'18px' }}>Training</Typography>
+                  <Typography
+                    variant="body1"
+                    align="center"
+                    style={{
+                      color: "black",
+                      fontWeight: "bold",
+                      fontSize: "18px",
+                    }}
+                  >
+                    Training
+                  </Typography>
                 </div>
               </Grid>
             </Grid>
@@ -97,35 +138,107 @@ const Home = () => {
               <Grid item>
                 <div className="service1-item-container">
                   <img src={image6} alt="Service 4" className="service-image" />
-                  <Typography variant="body1" align="center" style={{ color: 'black', fontWeight: 'bold',fontSize:'18px' }}>ESG</Typography>
+                  <Typography
+                    variant="body1"
+                    align="center"
+                    style={{
+                      color: "black",
+                      fontWeight: "bold",
+                      fontSize: "18px",
+                    }}
+                  >
+                    ESG
+                  </Typography>
                 </div>
               </Grid>
               <Grid item>
                 <div className="service1-item-container">
                   <img src={image5} alt="Service 5" className="service-image" />
-                  <Typography variant="body1" align="center" style={{ color: 'black', fontWeight: 'bold',fontSize:'18px' }}>Embedded Systems</Typography>
+                  <Typography
+                    variant="body1"
+                    align="center"
+                    style={{
+                      color: "black",
+                      fontWeight: "bold",
+                      fontSize: "18px",
+                    }}
+                  >
+                    Embedded Systems
+                  </Typography>
                 </div>
               </Grid>
             </Grid>
           </Grid>
         </Grid>
-        <Typography variant="h4" align="center" gutterBottom style={{marginTop:'40px', fontWeight: 'bold' }}>
+        <Typography
+          variant="h4"
+          align="center"
+          gutterBottom
+          style={{ marginTop: "40px", fontWeight: "bold" }}
+        >
           About Us
         </Typography>
-        <Grid container spacing={2} justifyContent="center" style={{ color: 'black', fontWeight: 'bold' }}>
+        <Grid
+          container
+          spacing={2}
+          justifyContent="center"
+          style={{ color: "black", fontWeight: "bold" }}
+        >
           <Grid item xs={12}>
             <div className="about-us-container">
               <div className="about-us-image">
-               <a href='/about'> <img src={officeBuildingImage} alt="Office Building" /></a>
-                <Typography variant="body1" align="center" style={{ color: 'black', fontWeight: 'bold',fontSize:'15px', cursor: 'pointer'  }} >Company Profile</Typography>
+                <a href="/about">
+                  {" "}
+                  <img src={officeBuildingImage} alt="Office Building" />
+                </a>
+                <Typography
+                  variant="body1"
+                  align="center"
+                  style={{
+                    color: "black",
+                    fontWeight: "bold",
+                    fontSize: "15px",
+                    cursor: "pointer",
+                  }}
+                >
+                  Company Profile
+                </Typography>
               </div>
               <div className="about-us-image">
-              <a href='/about'> <img src={timelineImage} alt="Timeline" /></a>
-                <Typography variant="body1" align="center" style={{ color: 'black', fontWeight: 'bold',fontSize:'15px',cursor: 'pointer' }} >Company History</Typography>
+                <a href="/about">
+                  {" "}
+                  <img src={timelineImage} alt="Timeline" />
+                </a>
+                <Typography
+                  variant="body1"
+                  align="center"
+                  style={{
+                    color: "black",
+                    fontWeight: "bold",
+                    fontSize: "15px",
+                    cursor: "pointer",
+                  }}
+                >
+                  Company History
+                </Typography>
               </div>
               <div className="about-us-image">
-              <a href='/about'> <img src={meetingImage} alt="Meeting" /></a>
-                <Typography variant="body1" align="center" style={{ color: 'black', fontWeight: 'bold',fontSize:'15px',cursor: 'pointer' }}>Our Resources</Typography>
+                <a href="/about">
+                  {" "}
+                  <img src={meetingImage} alt="Meeting" />
+                </a>
+                <Typography
+                  variant="body1"
+                  align="center"
+                  style={{
+                    color: "black",
+                    fontWeight: "bold",
+                    fontSize: "15px",
+                    cursor: "pointer",
+                  }}
+                >
+                  Our Resources
+                </Typography>
               </div>
             </div>
           </Grid>
